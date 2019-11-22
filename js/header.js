cc4f05c26bf4c980e5e776c2a1cf35fbdd6dc9ee
Axel Renard <arenard@dhcp149.adfinitas.fr>
2019-11-22 10:48:45 +0100

    header

---
+++ js/header.js
@@ -0,0 +1,15 @@
+
+if (window.innerWidth < 780) {
+    $("#titleHeader").attr("src", "assets/titleHeaderSmall.png");
+    $("#titleHeader").css("margin-left", "-173px");
+}
+
+$(window).resize(function () {
+    if (window.innerWidth < 780) {
+        $("#titleHeader").attr("src", "assets/titleHeaderSmall.png");
+        $("#titleHeader").css("margin-left", "-173px");
+    } else {
+        $("#titleHeader").attr("src", "assets/titleHeader.png");
+        $("#titleHeader").css("margin-left", "-389px");
+    }
+});
